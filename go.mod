b81558c4e23d076b4931b7c5dc512de1e1528c3a
David Tong <ytong@ebay.com>
2020-05-09 22:11:35 -0500

    Init Commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ytong82/go-modules-test
+
+go 1.14
+
+require github.com/elliotforbes/test-package v1.0.0
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+    "fmt"
+
+    sample "github.com/elliotforbes/test-package"
+)
+
+func main() {
+    fmt.Println("Hello World")
+    sample.MySampleFunc()
+}
